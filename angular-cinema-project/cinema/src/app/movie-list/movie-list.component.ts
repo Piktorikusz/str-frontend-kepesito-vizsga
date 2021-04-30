0caca6d2f66cdac6e2ec9386dcd503af740e3195
Piktorikusz <piktorikusz@gmail.com>
2021-04-30 12:57:06 +0200

    vege

--- angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
+++ angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Movie } from '../model/movie';
+import { HttpService } from '../service/http.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -9,14 +11,22 @@ import { Movie } from '../model/movie';
 export class MovieListComponent implements OnInit {
 
   movies: Movie[] = [];
+  list$: Observable<Movie[]> = this.httpService.getMovieList();
 
-  constructor() { }
+  constructor(private httpService: HttpService,) { 
+    
+  }
 
   ngOnInit(): void {
   }
 
   getMovies(): void {}
 
-  deleteMovie(id: number): any {}
+  
+  deleteMovie(id: number): void {
+    this.httpService.deleteMovie(id).subscribe(
+      () => this.list$ = this.httpService.getMovieList()
+    );
+  }
 
 }
--- angular-cinema-project/cinema/src/app/movie/movie.component.ts
+++ angular-cinema-project/cinema/src/app/movie/movie.component.ts
@@ -1,6 +1,7 @@
 import { AfterContentChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import {  FormGroup } from '@angular/forms';
 import { Movie } from '../model/movie';
+import { HttpService } from '../service/http.service';
 
 @Component({
   selector: 'app-movie',
@@ -11,16 +12,22 @@ export class MovieComponent implements OnInit {
 
   @ViewChild('mForm', { static: true }) movieForm: FormGroup;
   movie: Movie = new Movie();
+  
 
-  constructor() {
+  constructor(private httpService : HttpService) {
 
   }
 
   ngOnInit(): void {
   }
 
-  saveMovie(): any {
-    return {};
+  
+
+  saveMovie(movie: Movie): void {
+    // tslint:disable-next-line: deprecation
+    this.httpService.update(this.movie).subscribe(
+      movie => console.log(movie)
+    )
   }
 
 }
